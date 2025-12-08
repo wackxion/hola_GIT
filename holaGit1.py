e045c56cd37f0fc52b1cefb3f9cc6eabe117756c
wackxion <bra_wack@hotmail.com>
2025-12-07 21:03:58 -0300

    cuarto commit probando, ramas siguiendo la rama principal

--- holaGit1.py
+++ holaGit1.py
@@ -1,2 +1,8 @@
 print ("hola GIT")
 print("como aprendo git en 5 horas ")
+print("seguimos aprendiendo git")
+print("finalizando el aprendizaje de git")
+
+def suma(a, b):
+    return a + b
+print("la suma de 2 + 3 es:", suma(2, 3))
