883049ab1a685f0918de59269daa1d0687ba3970
wackxion <bra_wack@hotmail.com>
2025-12-17 17:44:45 -0300

    probando git hub

--- holaGit1.py
+++ holaGit1.py
@@ -5,7 +5,12 @@ print("finalizando el aprendizaje de git")
 
 def suma(a, b):
     return a + b
+
 print("la suma de 2 + 3 es:", suma(2, 3))
 print("la suma de 5 + 7 es:", suma(5, 7))
 print("la suma de 5 + 7 es:", suma(5, 7))
 print("la suma de 10 + 15 es:", suma(10, 15))
+
+
+print(suma(20, 30))
+print("finalizando el aprendizaje de git")
